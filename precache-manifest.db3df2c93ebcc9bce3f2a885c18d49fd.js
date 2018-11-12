ad89c8d465d0b8eacc9163f93c30c76d7514a9dc
Andrey Sloboda <sloboda.a@d2.digital>
2018-11-12 05:12:57 +0200

    Updates

---
+++ precache-manifest.db3df2c93ebcc9bce3f2a885c18d49fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3515f7f458f321cdd040126f7693d5d4",
+    "url": "/e-react/static/media/no-image.3515f7f4.png"
+  },
+  {
+    "revision": "9088bd889f3732fdba4b",
+    "url": "/e-react/static/js/runtime~main.9088bd88.js"
+  },
+  {
+    "revision": "36d37aa2c42177f1390a",
+    "url": "/e-react/static/js/main.36d37aa2.chunk.js"
+  },
+  {
+    "revision": "af17453917394d9413e2",
+    "url": "/e-react/static/js/1.af174539.chunk.js"
+  },
+  {
+    "revision": "36d37aa2c42177f1390a",
+    "url": "/e-react/static/css/main.06f2462d.chunk.css"
+  },
+  {
+    "revision": "e6edf37c8e5b7676b5e418866353deb0",
+    "url": "/e-react/index.html"
+  }
+];
